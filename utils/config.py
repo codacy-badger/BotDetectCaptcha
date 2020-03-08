4bd8a29f894834212eccb0d3af34c7d2ab57ebec
Egor Kostan <igorkostan@gmail.com>
2020-03-08 14:03:39 -0700

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -18,13 +18,40 @@ class Config:
 	def __init__(self):
 		params = get_args()
 
-		self._base_url = {
+		self.__base_url = {
 			'google': 'https://www.google.com/',
 			'integration': '',
 			'production': '',
 			'localhost': 'http://localhost:8080',
 		}[params['env'].lower()]
 
+		self.__is_headless = params['is_headless']
+
+		self.__browser = {
+			'chrome': 'chrome',
+			'edge': 'edge',
+			'firefox': 'mozilla'
+		}[params['browser'].lower()]
+
+		self.__print_run_config()
+
+	@property
+	def browser(self):
+		return self.__browser
+
 	@property
 	def base_url(self):
-		return self._base_url
+		return self.__base_url
+
+	@property
+	def is_headless(self):
+		return self.__is_headless
+
+	def __print_run_config(self):
+		"""
+		Prints run configurations
+		:return:
+		"""
+		print('\nRun configurations -> Browser: {}\n'
+		      'Run configurations -> Environment: {}\n'
+		      'Run configurations -> Is Headless: {}'.format(self.browser, self.base_url, self.is_headless))
