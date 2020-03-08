ab728b82d245eb452065d73bb206262704082a8b
Egor Kostan <igorkostan@gmail.com>
2020-03-08 14:19:54 -0700

    Create refresh_page.py

---
+++ utils/refresh_page.py
@@ -0,0 +1,29 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+import time
+
+
+def refresh_page(expected: str, page, sleep_time: int = 3):
+	"""
+	1. Check if page has expected title.
+	2. Trying to refresh web page in case page title dos not equal expected result
+	3. Raises exception in case page title dos not equal expected result
+	:param expected:
+	:param sleep_time:
+	:param page:
+	:return:
+	"""
+
+	counter = 0
+	while expected != page.title() and counter < 5:
+		print("\nERROR: expected page title {} != {}.\nTrying to refresh...\n".format(expected, page.title()))
+		page.driver.refresh()
+		counter += 1
+		time.sleep(sleep_time)
+
+	if expected != page.title():
+		raise Exception("\nERROR: This site can't be reached\n")
+
+	return None
