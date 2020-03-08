9ab381743c2f0ac21c7b7b3781053f752fbfdf5b
Egor Kostan <igorkostan@gmail.com>
2020-03-08 15:13:35 -0700

    Create base_page_model.py

---
+++ page_object_models/base_page_model.py
@@ -0,0 +1,145 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+from utils.config import Config
+from utils.driver import Driver
+
+
+class BasePageModel:
+	"""
+	The page object pattern intends creating
+	an object for each web page.
+
+	By following this technique a layer of separation
+	between the test code and technical implementation is created.
+	"""
+
+	def __init__(self,
+	             config: Config,
+	             driver: Driver,
+	             implicit_wait_time: int,
+	             explicit_wait_time: int):
+		self.__config = config
+		self.__url = config.base_url + BasePageContent.URL
+		self.__implicit_wait_time = implicit_wait_time
+		self.__explicit_wait_time = self.__set_explicit_wait(explicit_wait_time)
+		self.__driver = self.__set_driver(driver)
+		self.__set_implicit_wait(implicit_wait_time)
+
+	@property
+	def config(self):
+		"""
+		Returns current configurations:
+			base_url
+			browser
+			is_headless
+		:return:
+		"""
+		return self.__config
+
+	@property
+	def implicit_wait_time(self) -> int:
+		"""
+
+		:return:
+		"""
+		return self.__implicit_wait_time
+
+	@property
+	def explicit_wait_time(self) -> int:
+		return self.__explicit_wait_time
+
+	@staticmethod
+	def __set_driver(driver):
+		"""
+		Check if driver of type: selenium.webdriver
+		:param driver:
+		:return:
+		"""
+
+		# print('\nDriver type: {}'.format(type(driver)))  # debug only
+		if isinstance(driver, Driver):
+			return driver.get_driver()
+
+		return driver
+
+	def __set_implicit_wait(self, implicit_wait_time: int) -> None:
+		"""
+		The default value of time that can be set using Implicit wait is zero.
+		Its unit is in seconds.
+		Implicit wait remains associated with the web element until it gets destroyed.
+		:param implicit_wait_time:
+		:return:
+		"""
+		if type(implicit_wait_time) != int:
+			raise TypeError('\nERROR: wrong data type. Please set "implicit_wait_time" value as integer.\n')
+		self.__driver.implicitly_wait(implicit_wait_time)
+		return None
+
+	@staticmethod
+	def __set_explicit_wait(explicit_wait_time: int) -> int:
+		"""
+		The default value of explicit wait time.
+		Its unit is in seconds.
+		:param explicit_wait_time:
+		:return:
+		"""
+		if type(explicit_wait_time) != int:
+			raise TypeError('\nERROR: wrong data type. '
+			                'Please set "explicit_wait_time" value as integer.\n')
+		return explicit_wait_time
+
+	def go(self) -> None:
+		"""
+		Opens test web page
+		:return:
+		"""
+
+		self.__driver.get(self.__url)
+		self.__driver.maximize_window()
+		return None
+
+	def quit(self) -> None:
+		"""
+		Close web browser window (including all opened tabs)
+		:return:
+		"""
+		if self.__driver:
+			self.__driver.quit()
+		return None
+
+	def close(self) -> None:
+		"""
+		Close current tab
+		:return:
+		"""
+		if self.__driver:
+			self.__driver.close()
+		return None
+
+	@property
+	def driver(self):
+		"""
+		Returns Selenium webdriver object
+		:return:
+		"""
+		return self.__driver
+
+	@property
+	def title(self) -> str:
+		"""
+		Returns web page title
+		:return:
+		"""
+		title: str = self.__driver.title
+		return title
+
+	@property
+	def url(self) -> str:
+		"""
+		Returns current URL
+		:return:
+		"""
+		url: str = self.__driver.current_url
+		return url
