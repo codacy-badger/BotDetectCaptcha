2f4bfe2728b6d002f9a987610831631680144e21
Egor Kostan <igorkostan@gmail.com>
2020-03-07 18:52:57 -0800

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,37 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+"""Environment Configuration Class"""
+
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+"""Environment Configuration Class"""
+
+from utils.get_args_from_cli import get_args
+
+
+class Config:
+	"""
+	Environment Configuration Class
+
+	Source:
+	https://www.udemy.com/elegant-automation-frameworks-with-python-and-pytest
+	"""
+
+	def __init__(self):
+		params = get_args()
+
+		self._base_url = {
+			'': 'https://www.google.com/',
+			'integration': '',
+			'production': '',
+			'localhost': 'http://localhost:8080',
+		}[params['env'].lower()]
+
+	@property
+	def base_url(self):
+		return self._base_url
