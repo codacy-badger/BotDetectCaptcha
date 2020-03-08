eadc6c5fed0a51f5bda3eddd028c8a778f7e1a6f
Egor Kostan <igorkostan@gmail.com>
2020-03-08 14:16:22 -0700

    Create path_config.py

---
+++ utils/path_config.py
@@ -0,0 +1,12 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+class DriverPath:
+
+    CHROME_WEB_DRIVER_PATH = '\\drivers\\chrome\\win32\\chromedriver.exe'
+
+    EDGE_WEB_DRIVER_PATH = '\\drivers\\microsoft_edge\\win64\\MicrosoftWebDriver.exe'
+
+    MOZILLA_WEB_DRIVER_PATH = '\\drivers\\mozilla_geckodriver\\win64\\geckodriver.exe'
