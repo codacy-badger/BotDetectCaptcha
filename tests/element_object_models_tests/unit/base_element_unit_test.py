41de01926b7ce42f21d8a6ab01db5ee66fcb1f0e
Egor Kostan <igorkostan@gmail.com>
2020-03-08 14:46:18 -0700

    Create base_element_unit_test.py

---
+++ tests/element_object_models_tests/unit/base_element_unit_test.py
@@ -0,0 +1,10 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+import unittest
+
+
+class BaseElementUnitTestCase(unittest.TestCase):
+	def test_base_el_not_null(self):
+		pass
