509929b0f17992eb0faf0bc2809c37b6d728c3ab
Egor Kostan <igorkostan@gmail.com>
2020-03-08 20:57:03 -0700

    Update open_web_browser.py

--- utils/open_web_browser.py
+++ utils/open_web_browser.py
@@ -29,11 +29,14 @@ def open_web_browser(config: Config, page_model, page_content):
 	with allure.step("Open following web page: {}.".format(page_content.URL)):
 
 		driver = Driver(config=config, is_debug=True)
+
 		print('\nbase_url: {}\n'.format(config.base_url))
+
 		page = page_model(config=config,
 		                  driver=driver,
 		                  implicit_wait_time=5,
 		                  explicit_wait_time=10)
+
 		page.go()
 		get_http_status_code(page.url())
 		refresh_page(page_content.TITLE, page)
