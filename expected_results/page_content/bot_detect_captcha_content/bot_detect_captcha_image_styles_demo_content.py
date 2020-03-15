1968aff2e43eb007a073b0275bb4adec059b79b7
Egor Kostan <igorkostan@gmail.com>
2020-03-15 14:19:52 -0700

    Create bot_detect_captcha_image_styles_demo_content.py

---
+++ expected_results/page_content/bot_detect_captcha_content/bot_detect_captcha_image_styles_demo_content.py
@@ -0,0 +1,10 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+class BotDetectCaptchaImageStylesDemoPageContent:
+
+	URL = 'demos/image-styles/captcha-demo.aspx'
+
+	TITLE = 'BotDetect CAPTCHA Demo - Image Styles'
