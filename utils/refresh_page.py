4c8bea02b422eb6f2616447a549c2bb1bfd7dc7c
Egor Kostan <igorkostan@gmail.com>
2020-03-15 14:56:28 -0700

    Update refresh_page.py

--- utils/refresh_page.py
+++ utils/refresh_page.py
@@ -17,13 +17,13 @@ def refresh_page(expected: str, page, sleep_time: int = 3):
 	"""
 
 	counter = 0
-	while expected != page.title() and counter < 5:
-		print("\nERROR: expected page title {} != {}.\nTrying to refresh...\n".format(expected, page.title()))
+	while expected != page.title and counter < 5:
+		print("\nERROR: expected page title {} != {}.\nTrying to refresh...\n".format(expected, page.title))
 		page.driver.refresh()
 		counter += 1
 		time.sleep(sleep_time)
 
-	if expected != page.title():
+	if expected != page.title:
 		raise Exception("\nERROR: This site can't be reached\n")
 
 	return None
