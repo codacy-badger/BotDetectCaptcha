3eee4a176e4ea1f8259b6788f7aa9348db58605f
Egor Kostan <igorkostan@gmail.com>
2020-03-15 14:56:23 -0700

    Update bot_detect_base_element_test.py

--- tests/page_object_models_tests/integration_tests/bot_detect_captcha_integration_tests/bot_detect_base_element_test.py
+++ tests/page_object_models_tests/integration_tests/bot_detect_captcha_integration_tests/bot_detect_base_element_test.py
@@ -2,34 +2,52 @@
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
-
 import unittest
+import allure
 from tests.config import Config
-from utils.driver import Driver
 from page_object_models.bot_detect_captcha.bot_detect_captcha_image_styles_demo_page_model import \
-	BotDetectCaptchaImageStylesDemo
+	BotDetectCaptchaImageStylesDemoModel
+from expected_results.page_content.bot_detect_captcha_content.bot_detect_captcha_image_styles_demo_content import \
+	BotDetectCaptchaImageStylesDemoPageContent
+from utils.open_web_browser import open_web_browser
 
 
 class BaseElementIntegrationTestCase(unittest.TestCase):
 
-	def test_bot_detect_captcha_image_styles_demo_page_logo(self):
+	@classmethod
+	def setUpClass(cls):
+		cls.page = None
+		cls.config = Config()
 
-		config = Config()
+	@classmethod
+	def tearDownClass(cls):
+		with allure.step("Close Web Browser"):
+			if cls.page:
+				cls.page.quit()
+				cls.page = None
 
-		driver = Driver(config=config, is_debug=True).get_driver()
+	def setUp(self):
+		with allure.step("Initial data setup: {}".format(BotDetectCaptchaImageStylesDemoPageContent.URL)):
+			self.page_model = BotDetectCaptchaImageStylesDemoModel
+			self.page_context = BotDetectCaptchaImageStylesDemoPageContent
+			with allure.step("Open web browser"):
+				self.page = open_web_browser(config=self.config,
+				                             page_model=self.page_model,
+				                             page_content=self.page_context)
 
-		page = BotDetectCaptchaImageStylesDemo(config=config,
-		                                       driver=driver,
-		                                       explicit_wait_time=30,
-		                                       implicit_wait_time=5)
+	def tearDown(self):
+		with allure.step("Close current tab"):
+			if self.page:
+				self.page.close()
 
-		page.go()
+	def test_bot_detect_captcha_image_styles_demo_logo(self):
 
-		element = page.logo
+		logo = self.page.logo
+		self.assertIsNotNone(logo)
 
-		self.assertIsNotNone(element)
+	def test_bot_detect_captcha_image_styles_demo_url(self):
 
-		page.close()
+		url = self.page.url
+		expected = self.config.base_url + BotDetectCaptchaImageStylesDemoPageContent.URL
+		self.assertEqual(expected, url)
+
